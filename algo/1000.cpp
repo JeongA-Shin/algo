8e1d39c4c6c128e2212a243a1f6b46e1eecb2ecb
신정아 <jeonga@khu.ac.kr>
2022-07-04 16:52:36 +0900

    init study

---
+++ algo/1000.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/*
+* 나누어 입력받기
+* 
+* 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+첫째 줄에 A+B를 출력한다.
+
+*/
+
+
+int main() {
+
+	int a, b;
+
+	cin >> a >> b; //한 줄로 입력받기
+
+	cout << a + b << endl;
+
+	return 0;
+
+}
