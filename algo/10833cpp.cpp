434899ba0b90355ec0efcf4434ee6784c6a567d7
신정아 <jeonga@khu.ac.kr>
2022-07-11 19:24:19 +0900

    단순 연산

---
+++ algo/10833cpp.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	int remainder = 0;
+
+	int school_cnt;
+
+	cin >> school_cnt;
+
+
+	for (int i = 0; i < school_cnt; i++) {
+		int students, apples;
+
+		cin >> students >> apples;
+
+		remainder +=apples % students;
+	}
+
+	cout << remainder << endl;
+
+	return 0;
+
+}
