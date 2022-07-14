e057016fc3393f724dd5ad482ce206f4730b20c9
신정아 <jeonga@khu.ac.kr>
2022-07-14 19:15:45 +0900

    long long, 자료형 변환 연습 - long long to string, char to int

---
+++ algo/2577.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int cnt[11];
+
+int main() {
+
+	long long a, b, c;
+
+	cin >> a >> b >> c;
+
+	long long res = a * b * c;
+
+	string mul_res = to_string(res);
+
+	for (int i = 0; i < mul_res.size(); i++) {
+		int index = mul_res[i] - '0'; //char to int
+		cnt[index]++;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << cnt[i] << endl;
+	}
+
+
+
+}
