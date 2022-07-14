59342e6ca34bac1ff3f2b2e4712533da0401e9f5
신정아 <jeonga@khu.ac.kr>
2022-07-14 19:05:42 +0900

    string to int, 삼항연산자

---
+++ algo/2908.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+	string a, b;
+
+	cin >> a >> b;
+
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+
+	//string -> int
+	int num_a = stoi(a);
+	int num_b = stoi(b);
+
+	int res = num_a > num_b ? num_a : num_b;
+
+	cout << res << endl;
+}
