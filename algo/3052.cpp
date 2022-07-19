b4ccd0f52d137b9a9a64f3dff8d5948700c19daa
신정아 <jeonga@khu.ac.kr>
2022-07-19 18:37:18 +0900

    집합은 중복을 허용하지 않는다

---
+++ algo/3052.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+set<int> s;
+
+int main() {
+
+	//집합은 중복을 허용하지 않는다
+	for (int i = 0; i < 10; i++) {
+		int num;
+		cin >> num;
+		int remainder = num % 42;
+
+		s.insert(remainder);
+
+	}
+
+	/*
+	set<int>::iterator iter;
+	for (iter = s.begin(); iter != s.end(); iter++) {
+		cout << *iter << endl;
+	}
+	*/
+
+	cout << s.size() << endl;
+}
