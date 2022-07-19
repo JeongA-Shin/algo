258269ef7dd23afddbc87395cf127228915ae4ad
신정아 <jeonga@khu.ac.kr>
2022-07-19 18:26:49 +0900

    map iterator 활용,map은 키 기준 오름차순으로 정렬됨

---
+++ algo/2822.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+map<int, int> m;
+
+int num[10];
+
+// !! map은 디폴트가 "key" 오름차순으로 정렬됨!
+
+int main() {
+
+	for (int i = 0; i < 8; i++) {
+		int score;
+		cin >> score;
+
+		m.insert({ score, i+1 }); // 점수,문제 번호
+
+	}
+
+
+	map<int, int>::iterator iter = m.end();
+	int sum = 0;
+
+	int cnt = 5;
+	while (cnt > 0) {
+		iter--;
+		sum += iter->first;
+		//cout << iter->second << endl;
+		num[cnt] = iter->second;
+		cnt--;
+	}
+
+	sort(num, num + 10);
+
+	cout << sum << endl;
+	for (int i = 5; i < 10; i++) {
+		cout << num[i] << " ";
+	}
+	cout << endl;
+}
