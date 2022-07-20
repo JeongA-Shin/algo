95f9f722b3db0be46a080a82ff39fc48e0484d3f
신정아 <jeonga@khu.ac.kr>
2022-07-20 17:54:17 +0900

    단순하게 생각해서 쉽게 풀렸던 문제

---
+++ algo/5054.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[101];
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int stores;
+		cin >> stores;
+
+
+		for (int j = 0; j < stores; j++) {
+			int distance;
+			cin >> distance;
+			arr[j] = distance;
+		}
+
+		sort(arr, arr + stores); //오름차순
+
+		int res = (arr[stores - 1] - arr[0]) * 2;
+
+		cout << res << endl;
+	}
+
+}
