4092722e8e91808158d162eb7b40f5b21893da0e
신정아 <jeonga@khu.ac.kr>
2022-07-31 17:51:30 +0900

    그냥 단순 브론즈 문제

---
+++ algo/2902.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string name;
+
+	cin >> name;
+
+	string result;
+	result+= name[0];
+
+	for (int i = 1; i < name.length(); i++) {
+		if (name[i] == '-') {
+			result += name[i + 1];
+		}
+	}
+
+	cout << result << endl;
+
+}
