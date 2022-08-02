00d7dd10b77be62379e22f26e4c6e8940b4c0b5a
신정아 <jeonga@khu.ac.kr>
2022-08-02 22:58:02 +0900

    큐 기본

---
+++ algo/2164.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<int> que;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i < n+1; i++) {
+		que.push(i);
+	}
+
+	while (que.size() > 1) {
+		que.pop(); //제일 위에 있는 카드를 바닥에 버린다.
+		//제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+		int element = que.front();
+		que.pop();
+		que.push(element);
+		
+	}
+
+	cout << que.front() << endl;
+
+}
