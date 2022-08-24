009890010b5561ae5dff7023269cf98d18f36d76
신정아 <jeonga@khu.ac.kr>
2022-08-24 18:01:44 +0900

    자료구조 - 집합,set

---
+++ algo/14425.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+#define fastio() ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+
+set<string> stringSet;
+
+int main() {
+    fastio();
+
+    // 여기에 구현
+
+    int n, m;
+    cin >> n >> m;
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+        stringSet.insert(word);
+    }
+
+    for (int i = 0; i < m; i++) {
+        string my_word;
+        cin >> my_word;
+
+        auto iter = stringSet.find(my_word);
+        if (iter != stringSet.end()) { //존재한다면
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
