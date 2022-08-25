bbfb375d4f2aaff2c53be37141ffaf3d83fcf213
신정아 <jeonga@khu.ac.kr>
2022-08-25 20:41:40 +0900

    템플릿

---
+++ algo/1_template.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <algorithm>
+#define fastio() ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+
+
+int main() {
+    fastio();
+    
+    // 여기에 구현
+}
